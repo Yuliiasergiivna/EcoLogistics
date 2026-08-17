0f52d36c5221926db4f9e142204bf7a7b32783a8
Yuliia <krempolska@gmail.com>
2026-08-17 15:43:34 +0200

    AddAllBlocks

---
+++ Migrations/20260817133741_AddAllBlock.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoLogistics.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllBlock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
